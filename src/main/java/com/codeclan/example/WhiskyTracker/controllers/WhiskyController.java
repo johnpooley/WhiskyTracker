415959a40fad7619828ce4f9a5ca47e05f7c20e3
JohnPooley <thevinylvandal@hotmail.com>
2020-04-01 18:16:51 +0100

    can find whiskey by age at /whiskies

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,7 +20,10 @@ public class WhiskyController {
     WhiskyRepository whiskyRepository;
 
     @GetMapping
-    public ResponseEntity<List<Whisky>> getAllWhiskies(){
+    public ResponseEntity<List<Whisky>> getWhiskiesFilterByAge(@RequestParam(name = "age",required = false)Integer age){
+        if(age!=null){
+            return new ResponseEntity<>(whiskyRepository.findWhiskyByAge(age),HttpStatus.OK);
+        }
         return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
     }
 
--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepository.java
@@ -4,6 +4,10 @@ import com.codeclan.example.WhiskyTracker.models.Whisky;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WhiskyRepository extends JpaRepository<Whisky, Long>, WhiskyRepositoryCustom {
+
+    List<Whisky> findWhiskyByAge(int age);
 }
